935ad738c9d1d28ee44a8c49a2e854201b1f0bef
joseserran <joseangelserranomedina@gmail.com>
2019-06-14 20:21:02 +0200

    version1 DMA se supone funcional

--- PIC/DMA.vhd
+++ PIC/DMA.vhd
@@ -30,7 +30,7 @@ end DMA;
 
 architecture behavior of DMA is
 
- type estados is (idle,transmitiendo_buffer_1,transmitiendo_buffer_2, recibiendo_LSB, recibiendo_middle, recibiendo_MSB, comando_completo);
+ type estados is (idle,esperandoEnvio,pidiendoBusesRecepcion,lecturaDato1,lecturaDato2,lecturaDato3,escribirFF,envioDato1, envioDato2);
  signal estado_a, estado_s : estados;
  --type estados2 is (idle, transmitiendo, recibiendo);--una segunda maquina de estados
 signal dato_1_enviado, dato_2_enviado : std_logic := '0';
@@ -63,169 +63,112 @@ reloj_ram: process(CLK100MHZ, reset)  -- no reset
        end if;
 end process;
 
-FSM: process(estado_a, dato_1_enviado, dato_2_enviado, RX_empty, RX_Full,peticion_send_comm, send_comm,TX_RDY,
-    ACK_out,DMA_ACK,RCVD_Data, databus)
+FSM: process(estado_a,RX_empty, RX_Full,send_comm,TX_RDY, ACK_out,DMA_ACK,RCVD_Data, databus)
 begin
 
         
     case estado_a is
     
-     when idle =>
-     
-     READY <= '1';--DMA listo para funcionar
-     OE <= 'Z';--en espera OE en alta impedancia
-     Write_en <= 'Z'; --indicacion de escritura para la RAM
-     Address <= "ZZZZZZZZ";--libera el uso de la direccion de memoria
-     Databus <= "ZZZZZZZZ";--libera bus de datos del sistema
-     dato_1_enviado <= '0';
-     dato_2_enviado <= '0';
-     valid_D <= '1';
-     DMA_RQ <= '0';
-     oe <= '0';
-     peticion_send_comm <= send_comm;--volcamos la peticion a una variable para terminar el proceso
-     data_read <= '0'; --peticion de lectura de un nuevo dato desde el rs232
-     tx_data <= "ZZZZZZZZ";   
-     write_en <='0';
-       
-     --------------------CORREGIR EL CONCEPTO DE RX_EMPTY Y RX_FULL
-        if peticion_Send_comm = '1' and TX_RDY ='1' then -- if RX_full ='0' and RX_Empty = '0' and peticion_Send_comm = '1' and TX_RDY ='1' then
-         estado_s <= transmitiendo_buffer_1;
+        when idle =>
          
-         
---        elsif peticion_Send_comm = '0' and TX_RDY ='1' then -- elsif RX_full ='0' and RX_Empty = '1' and peticion_Send_comm = '0' and TX_RDY ='1' then
---        estado_s <= idle;
-        
-        
-        elsif RX_Empty = '0' and peticion_Send_comm = '0' and TX_RDY ='1' then --RX_Full = '1' and RX_Empty = '0' and peticion_Send_comm = '0' and TX_RDY ='1' then
-        estado_s <= recibiendo_LSB;
-        
-        
---        elsif RX_Full = '0' and RX_Empty = '0' and peticion_Send_comm = '0' and TX_RDY ='1' then
---        estado_s <= idle;
-        
-        
-        else 
-        estado_s <= idle;
-        
-        end if;
-        
-      
-        
-    when transmitiendo_buffer_1 =>
-    Address <= "00000101";
-   TX_Data <= Databus;--
-   oe <= '1'; --cargamos datos de la RAM al rs232
-   READY <= '0';--DMA en uso
---   dato_1_enviado <= '1';
-   
-    
---    if  dato_1_enviado = '1' and ACK_out = '0' then--ACK_out activa a nivel bajo
---             estado_s <= transmitiendo_buffer_2;
---             READY <= '0';--DMA en uso
---             valid_D <= '0'; --validacion del dato enviado al tranmisor RS232, low cuando es valido
-
-    if  ACK_out = '0' and peticion_Send_comm = '1' then--ACK_out activa a nivel bajo
-             estado_s <= transmitiendo_buffer_2;
---             READY <= '0';--DMA en uso
-             valid_D <= '0'; --validacion del dato enviado al tranmisor RS232, low cuando es valido
+             READY <= '1';--DMA listo para funcionar
+             OE <= 'Z';--en espera OE en alta impedancia
+             Write_en <= 'Z'; --indicacion de escritura para la RAM, en espera en alta impedancia
+             Address <= "ZZZZZZZZ";--libera el uso de la direccion de memoria, en espera en alta impedancia
+             Databus <= "ZZZZZZZZ";--libera bus de datos del sistema, en espera en alta impedancia
+             valid_D <= '1';
+             DMA_RQ <= '0';
              
-     elsif   ACK_out = '1' and peticion_Send_comm = '1' then--si aun el rs232 no ha adquirido el dato pero me siguen pidiendo mandar comando, lo espero
-            estado_s <= transmitiendo_buffer_1;
-            valid_D <= '1';--no validado el dato
-     else
-                estado_s <= idle;
-                valid_D <= '1';--no validado el dato
-     end if;
-        
-    when transmitiendo_buffer_2=>
-      Address <= "00000100";
-      TX_Data <= Databus;--
-      oe <= '1'; --cargamos datos de la RAM al rs232
---      dato_2_enviado <= '1';
-      
---       if  dato_2_enviado = '1' and ACK_out = '0'  then--ACK_out activa a nivel bajo
---                estado_s <= idle;
---                READY <= '0';--DMA en uso
---                valid_D <= '1'; --validacion del dato enviado al tranmisor RS232
-
-      if  ACK_out = '0' and peticion_Send_comm = '1' then--ACK_out activa a nivel bajo
-                estado_s <= idle;
---                READY <= '0';--DMA en uso
-                valid_D <= '0'; --validacion del dato enviado al tranmisor RS232, activo a nivel bajo
-             
-       elsif   ACK_out = '1' and peticion_Send_comm = '1' then--si aun el rs232 no ha adquirido el dato pero me siguen pidiendo mandar comando, lo espero
-                estado_s <= transmitiendo_buffer_2;
-                valid_D <= '1';--no validado el dato         
-        else 
-               estado_s <= idle;
-               valid_D <= '1';--no validado el dato
-        end if;
-      
-    when recibiendo_LSB=>
-        DMA_RQ <= '1'; --pedimos los buses al procesador
-        READY <= '0';--la DMA ya no esta ociosa
-    
-            if DMA_ACK = '1' and rx_empty = '0' and TX_RDY = '1' then--el procesador ha cedido el bus y la fifo NO esta vacia
-            
-            Databus<=RCVD_Data;
-            data_read <= '1'; --peticion de lectura del rs232
-            address <= "00000010"; --volcamos a la direccion de memoria buffer LSB
-            estado_s<=recibiendo_middle;
-            write_en <= '1';--habilitacion de escritura para la ram
+             peticion_send_comm <= send_comm;--volcamos la peticion a una variable para terminar el proceso
+             data_read <= '0'; --peticion de lectura de un nuevo dato desde el rs232
+             tx_data <= "ZZZZZZZZ"; 
+               
+             --version añadido en 14 de junio 2019
+             if Send_comm = '1' then
+                READY <= '0';--DMA en uso, ponermos a 0 durante funcionamiento
+                estado_s <= esperandoEnvio;
                 
---            elsif DMA_ACK = '0' and rx_empty = '0' and TX_RDY = '1' then--el procesador no ha cedido aun el bus y la fifo NO esta vacia
---            estado_s <= recibiendo_LSB;
-        
-            elsif  rx_empty = '1' then--el procesador no ha cedido aun el bus y la fifo SI esta vacia
+             elsif RX_Empty = '0' then
+                READY <= '0';--DMA en uso, ponermos a 0 durante funcionamiento
+                estado_s <= pidiendoBusesRecepcion;
+                
+             else
+                estado_s <= idle;
+             end if;
+    
+        when pidiendoBusesRecepcion =>
+            READY <= '0';--DMA en uso, ponermos a 0 durante funcionamiento
+            DMA_RQ <= '1'; --se mantiene al procesador solicitud de uso de buses
+            if DMA_ACK = '1' then
+                estado_s <= lecturaDato1;
+            end if;
+            
+        when lecturaDato1 =>
+            READY <= '0';--DMA en uso, ponermos a 0 durante funcionamiento
+            DMA_RQ <= '1'; --se mantiene al procesador solicitud de uso de buses
+            data_read <= '1'; --peticion de lectura del rs232
+            Databus<=RCVD_Data; --volcamos datos leidos en el bus de datos
+            address <= "00000010"; --volcamos a la direccion de memoria buffer LSB
+            Write_en <= '1';--habilitacion de escritura para la ram
+            estado_s <= lecturaDato2;
+    
+        when lecturaDato2 =>
+            READY <= '0';--DMA en uso, ponermos a 0 durante funcionamiento
+            DMA_RQ <= '1'; --se mantiene al procesador solicitud de uso de buses
+            data_read <= '1'; --peticion de lectura del rs232
+            Databus<=RCVD_Data; --volcamos datos leidos en el bus de datos
+            address <= "00000001"; --volcamos a la direccion de memoria buffer middle byte
+            Write_en <= '1';--habilitacion de escritura para la ram
+            estado_s <= lecturaDato3;
+    
+        when lecturaDato3 =>
+            READY <= '0';--DMA en uso, ponermos a 0 durante funcionamiento
+            DMA_RQ <= '1'; --se mantiene al procesador solicitud de uso de buses
+            data_read <= '1'; --peticion de lectura del rs232
+            Databus<=RCVD_Data; --volcamos datos leidos en el bus de datos
+            address <= "00000000"; --volcamos a la direccion de memoria buffer MSB
+            Write_en <= '1';--habilitacion de escritura para la ram
+            estado_s <= escribirFF;
+            
+        when escribirFF =>
+            READY <= '0';--DMA en uso, ponermos a 0 durante funcionamiento
+            DMA_RQ <= '1'; --se mantiene al procesador solicitud de uso de buses
+            address <= "00000000"; --volcamos a la direccion de memoria NEW_INST
+            Databus <= "11111111"; --valor 0xFF
+            Write_en <= '1';--habilitacion de escritura para la ram
             estado_s <= idle;
             
-            else 
-            estado_s <= recibiendo_LSB;
-            write_en <= '0';--mientras que no podamos escribir
-            end if; 
             
-     when recibiendo_middle=>
-     
-     if DMA_ACK = '1' and rx_empty = '0' and TX_RDY = '1' then--el procesador ha cedido el bus y la fifo NO esta vacia
-     
-     Databus<=RCVD_Data;
-     data_read <= '1'; --peticion de lectura del rs232
-     address <= "00000001"; --volcamos a la direccion de memoria buffer LSB
-     estado_s<=recibiendo_MSB;
-     write_en <= '1';--habilitacion de escritura para la ram
-         
-     elsif rx_empty = '1' then--el procesador no ha cedido aun el bus y la fifo SI esta vacia --DMA_ACK = '0' and 
-     estado_s <= idle;
-     
-     else 
-     estado_s <= recibiendo_middle;
-     write_en <= '0';--mientras que no podamos escribir
-     end if; 
-     
-     when recibiendo_MSB=>
-       
-         if DMA_ACK = '1' and rx_empty = '0' and TX_RDY = '1' then--el procesador ha cedido el bus y la fifo NO esta vacia
-     
-     Databus<=RCVD_Data;
-     data_read <= '1'; --peticion de lectura del rs232
-     address <= "00000000"; --volcamos a la direccion de memoria buffer LSB
-     estado_s<=comando_completo;
-     write_en <= '1';--habilitacion de escritura para la ram
-         
-     elsif rx_empty = '1' then--el procesador no ha cedido aun el bus y la fifo SI esta vacia
-     estado_s <= idle;
-     
-     else 
-     estado_s <= recibiendo_MSB;
-     write_en <= '0';--mientras que no podamos escribir
-     end if; 
-         
-     when comando_completo=>
-     Databus<="11111111"; --ponemos valor 0xFF
-     address <= "00000011"; --en registro NEW_INST de la RAM
-     estado_s<=idle;
-     write_en <= '1';--habilitacion de escritura para la ram
-           
+        when esperandoEnvio =>
+            READY <= '0';--DMA en uso, ponermos a 0 durante funcionamiento
+            if TX_RDY = '1' then
+                estado_s <= envioDato1;
+            end if;
+        
+        when envioDato1 =>
+            READY <= '0';--DMA en uso, ponermos a 0 durante funcionamiento 
+            Address <= "00000101"; --byte menos significativo 
+            OE <= '0'; -- habilitacion de salida de la ram
+            TX_DATA <= Databus; --volcamos databus en datos a mandar a rs232
+            Valid_d <= '0'; --Validación del dato de entrada por parte del sistema cliente. Activa a nivel bajo.
+            
+            if ACK_out = '1' then
+                estado_s <= envioDato2;
+            end if;
+            
+        when envioDato2 =>
+            READY <= '0';--DMA en uso, ponermos a 0 durante funcionamiento 
+            Address <= "00000100"; --byte mas significativo 
+            OE <= '0'; -- habilitacion de salida de la ram
+            TX_DATA <= Databus; --volcamos databus en datos a mandar a rs232
+            Valid_d <= '0'; --Validación del dato de entrada por parte del sistema cliente. Activa a nivel bajo.
+            
+            if ACK_out = '1' then
+                estado_s <= idle;
+            end if;
+                 
+        
+               
     end case;
 end process;
 
